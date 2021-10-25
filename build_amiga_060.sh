42b8c577736d7dca4cc69d5e149d1dd9f94e59dc
BSzili <col.lawrence@gmail.com>
2021-10-25 19:33:37 +0200

    remove the unused netinclude path from build_amiga_060.sh

--- build_amiga_060.sh
+++ build_amiga_060.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-make CC="m68k-amigaos-gcc -noixemul -m68060 -m68881 -fbbb=- -fno-peephole2 -Inetinclude" CXX="m68k-amigaos-g++ -noixemul -m68060 -m68881 -fbbb=- -fno-peephole2 -Inetinclude" AS="vasm -Fhunk -quiet -phxass -m68060 -DM68060" AR=m68k-amigaos-ar RANLIB=m68k-amigaos-ranlib PLATFORM=AMIGA AMISUFFIX=".060" $*
+make CC="m68k-amigaos-gcc -noixemul -m68060 -m68881 -fbbb=- -fno-peephole2" CXX="m68k-amigaos-g++ -noixemul -m68060 -m68881 -fbbb=- -fno-peephole2" AS="vasm -Fhunk -quiet -phxass -m68060 -DM68060" AR=m68k-amigaos-ar RANLIB=m68k-amigaos-ranlib PLATFORM=AMIGA AMISUFFIX=".060" $*
