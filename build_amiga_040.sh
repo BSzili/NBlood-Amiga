5575edc73d790df9020b82b6502123e94b5f1365
Szilárd Biró <col.lawrence@gmail.com>
2022-09-03 15:34:14 +0200

    build the executables with -ffast-math

--- build_amiga_040.sh
+++ build_amiga_040.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-make CC="m68k-amigaos-gcc -noixemul -m68040 -m68881 -fbbb=- -fno-peephole2" CXX="m68k-amigaos-g++ -noixemul -m68040 -m68881 -fbbb=- -fno-peephole2" AS="vasm -Fhunk -quiet -phxass -m68040" AR=m68k-amigaos-ar RANLIB=m68k-amigaos-ranlib PLATFORM=AMIGA AMISUFFIX=".040" $*
+make CC="m68k-amigaos-gcc -noixemul -m68040 -m68881 -fbbb=- -fno-peephole2 -ffast-math" CXX="m68k-amigaos-g++ -noixemul -m68040 -m68881 -fbbb=- -fno-peephole2 -ffast-math" AS="vasm -Fhunk -quiet -phxass -m68040" AR=m68k-amigaos-ar RANLIB=m68k-amigaos-ranlib PLATFORM=AMIGA AMISUFFIX=".040" $*
